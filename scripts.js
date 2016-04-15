0fa082a07fa79ae0fc7fb1c4953bbd1ce6fa8408
Bryan Willis <bryanwillis7@gmail.com>
2016-04-15 07:30:59 -0400

    Details finess
    Got a few points of critical feedback! Made sure to work on them a
    little bit. Added some jquery to highlight the active section upon
    scrolling. Took some fidgiting, but it works universally across all
    devices. Further edited some strange spacing issues.

--- scripts.js
+++ scripts.js
@@ -30,7 +30,26 @@ $(document).ready(function(){
 		}
 		return(false); 
 	})
-
+	
+	//highlight on scroll/click
+	
+	$('nav li a').on('click', function(event) {
+		$(this).parent().find('a').removeClass('active');
+		$(this).addClass('active');
+	});
+	
+	var scrollBottom = $(window).scrollTop() + $(window).height()
+	
+	$(window).on('scroll', function() {
+		$('.target').each(function() {
+			if($(window).scrollTop()+$(window).height() >= $(this).offset().top + 300) {
+				var id = $(this).attr('id');
+				$('nav a').removeClass('active');
+				$('nav a[href="#'+ id +'"]').addClass('active');
+			}
+		});
+	
+	});
 
 
 
